ee081608f27fad504615a826ab8a6efd350d3e89
sowmi11 <131943083+sowmi11@users.noreply.github.com>
2024-02-28 10:01:30 +0530

    Add files via upload

---
+++ Utilities/CustomException.java
@@ -0,0 +1,11 @@
+package custom;
+
+@SuppressWarnings("serial")
+public class CustomException extends Exception {
+	public CustomException(String message,Throwable cause){
+		super (message,cause);
+		}
+		public CustomException(String message){
+		super (message);
+		}
+		}
