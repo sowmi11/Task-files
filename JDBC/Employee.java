3f2278d9b3eeee8a5bf7dac6964b7135e0c7fc90
sowmi11 <131943083+sowmi11@users.noreply.github.com>
2024-02-28 09:58:04 +0530

    Add files via upload

---
+++ JDBC/Employee.java
@@ -0,0 +1,62 @@
+package dbtask;
+public class Employee {
+	private String name;
+	private long mobile;
+	private String email;
+	private String department;
+	private String dep_name;
+	private int age;
+	private String relation;
+	private int employee_id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getMobile() {
+		return mobile;
+	}
+	public void setMobile(long mobile) {
+		this.mobile = mobile;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getDep_name() {
+		return dep_name;
+	}
+	public void setDep_name(String dep_name) {
+		this.dep_name = dep_name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getRelation() {
+		return relation;
+	}
+	public void setRelation(String relation) {
+		this.relation = relation;
+	}
+	public int getEmployee_id() {
+		return employee_id;
+	}
+	public void setEmployee_id(int employee_id) {
+		this.employee_id = employee_id;
+	}
+}
+
+
+
